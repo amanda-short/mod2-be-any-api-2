6c7b2e6b0fe544b462178df634671d71ca77f628
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-03 09:22:23 -0700

    list route 1 test built - failed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,22 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
+const { university } = require('../lib/uni-data.js');
 // const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('university routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/university should return list of universities', async () => {
+    const res = await request(app).get('/university');
+    const expected = university.map((university) => {
+      return { id: university.id, school: university.school };
+    });
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
