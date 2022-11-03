b8e53e59a9224eb8fbb3be41914f35f83cc90cb1
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-03 11:04:35 -0700

    list route 1 built, tested, passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,9 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
+const app = require('../lib/app');
+
 const { university } = require('../lib/uni-data.js');
-// const app = require('../lib/app');
 
 describe('university routes', () => {
   beforeEach(() => {
@@ -11,9 +12,10 @@ describe('university routes', () => {
 
   it('/university should return list of universities', async () => {
     const res = await request(app).get('/university');
-    const expected = university.map((university) => {
-      return { id: university.id, school: university.school };
+    const expected = university.map((uni) => {
+      return { id: uni.id, school: uni.school };
     });
+    
     expect(res.body).toEqual(expected);
   });
 
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS team;
+
+CREATE TABLE team (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    school VARCHAR NOT NULL,
+    mascot VARCHAR NOT NULL,
+    location VARCHAR NOT NULL
+);
+
+INSERT INTO team (school, mascot, location)
+VALUES 
+('University of Oregon', 'Duck', 'Eugene Oregon'),
+('University of Southern California', 'Trojan', 'Los Angeles California'),
+('University of California Los Angeles', 'Bruin', 'Los Angeles California'),
+('University of Utah', 'Utes', 'Salt Lake City Utah'),
+('University of Washington', 'Husky', 'Seattle Washington'),
+('Arizona Sate University', 'Sun Devil', 'Tempe Arizona'),
+('University of Arizona', 'Wildcat', 'Tucson Arizona'),
+('Stanford University', 'Standford', 'Standford California'),
+('Oregon State University', 'Beaver', 'Corvallis Oregon'),
+('University of Colorado', 'Buffalo', 'Boulder Colorado'),
+('University of California Berkeley', 'Bear', 'Barkeley California'),
+('Washington State University', 'Cougar', 'Pullman Washington');
